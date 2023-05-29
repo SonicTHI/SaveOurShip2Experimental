e7db2024cabdae2be764710f3ec70721b6dc52f3
SonicTHI <44506034+SonicTHI@users.noreply.github.com>
2023-05-29 04:46:42 +0200

    V88f2

--- Source/1.4/PlaceWorker/PlaceWorker_ShipHull.cs
+++ Source/1.4/PlaceWorker/PlaceWorker_ShipHull.cs
@@ -6,7 +6,7 @@ namespace RimWorld
 {
 	public class PlaceWorker_ShipHull : PlaceWorker
 	{
-		//not on ship hull, not under any building that blocks path
+		//not under mountain
 		public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
 		{
 			CellRect occupiedRect = GenAdj.OccupiedRect(loc, rot, def.Size);
@@ -14,18 +14,6 @@ namespace RimWorld
 			{
 				if (vec.Fogged(map) || map.roofGrid.RoofAt(loc) == RoofDefOf.RoofRockThick)
 					return false;
-				foreach (Thing t in vec.GetThingList(map))
-				{
-					if (t is Building b)
-					{
-						if (b.def.passability == Traversability.Impassable || b.def.building.shipPart || b is Building_Door || b.Faction != Faction.OfPlayer || (b.TryGetComp<CompForbiddable>()?.Forbidden ?? false))
-							return false;
-					}
-					else if (t is Blueprint_Build) //no idea why this cant be checked for def.shipPart, etc.
-					{
-						return false;
-					}
-				}
 			}
 			return true;
 			/*
---
+++ Source/1.4/PlaceWorker/PlaceWorker_ShipPlating.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Verse;
+using SaveOurShip2;
+
+namespace RimWorld
+{
+	public class PlaceWorker_ShipPlating : PlaceWorker
+	{
+		//not on ship hull, not under any building that blocks path
+		public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			CellRect occupiedRect = GenAdj.OccupiedRect(loc, rot, def.Size);
+			foreach (IntVec3 vec in occupiedRect)
+			{
+				if (vec.Fogged(map) || map.roofGrid.RoofAt(loc) == RoofDefOf.RoofRockThick)
+					return false;
+				foreach (Thing t in vec.GetThingList(map))
+				{
+					if (t is Building b)
+					{
+						if (b.def.passability == Traversability.Impassable || b.def.building.shipPart || b is Building_Door || b.Faction != Faction.OfPlayer || (b.TryGetComp<CompForbiddable>()?.Forbidden ?? false))
+							return false;
+					}
+					else if (t is Blueprint_Build) //no idea why this cant be checked for def.shipPart, etc.
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
+}
